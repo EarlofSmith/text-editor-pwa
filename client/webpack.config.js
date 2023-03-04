6914823ffae6d6605c335cdde6aacad3500a22ec
EarlofSmith <r1smith3333@gmail.com>
2023-03-04 13:49:16 -0700

    added to install and webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,11 +18,58 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      // add webpack html plugin
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'JATE'
+      }),
+
+      new InjectManifest({
+          swSrc: './src-sw.js',
+          swDest: 'src-sw.js',
+      }),
+
+      // https://www.npmjs.com/package/webpack-pwa-manifest
+      // manifest json file is created here
+      new WebpackPwaManifest({
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description: 'Text editor that can be used offline',
+        background_color: 'black',
+        crossorigin: null,
+        start_url:'/',
+        publicPath: '/',
+        fingerprints: false,
+        icons: [
+          {
+            // takes the image and resizes it to designated sizes
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons')
+          },
+        ]
+      })
     ],
 
     module: {
       rules: [
+        // css loader
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        // babel loader
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        }
         
       ],
     },
